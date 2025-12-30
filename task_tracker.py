0b977de926f700ea6fd845c15644f861feadf0b0
Justin Anderson <justin.ta@outlook.com>
2025-12-29 21:46:51 -0700

    update_task, mark_task

--- task_tracker.py
+++ task_tracker.py
@@ -51,8 +51,10 @@ task-cli list in-progress
 import argparse
 import json
 
+from datetime import datetime
 from pathlib import Path
 
+### UTILS ###
 def parse_args():
     parser = argparse.ArgumentParser(description="Raodmap.sh Task Tracker")
     subparsers = parser.add_subparsers(dest='command', help='subcommand help')
@@ -70,37 +72,56 @@ def parse_args():
     delete_parser = subparsers.add_parser('delete', help='update help')
     delete_parser.add_argument('id', type=str, help='Task ID to delete')
 
+    mark_parser = subparsers.add_parser('mark', help='mark help')
+    mark_parser.add_argument('id', type=int, help='id help')
+    mark_parser.add_argument('status', choices=status_choices, help='status help')
+
     update_parser = subparsers.add_parser('update', help='update help')
-    update_parser.add_argument('--id', type=int, help='id help')
-    update_parser.add_argument('--status', type=str, choices=status_choices, help='status help')
+    update_parser.add_argument('id', type=int, help='id help')
+    update_parser.add_argument('--title', type=str, help='title help')
+    update_parser.add_argument('--description', type=str, help='description help')
 
     list_parser = subparsers.add_parser('list', help='list help')
-    list_parser.add_argument('status', type=str, choices=status_choices.append('all'), help='list status help')
+    list_choices = status_choices + ['all']
+    list_parser.add_argument('status', type=str, nargs='?', default='all', choices=list_choices, help='list status help')
 
     return parser.parse_args()
 
+
+def open_json_file(path, op='r', data=None):
+    if op == 'r':
+        with open(path, op) as f:
+            json_file = json.load(f)
+        return json_file
+    if op == 'w' and data:
+        with open(path, op) as f:
+            json.dump(data, f, indent=4)
+
+### END UTILS ###
 
 def add_task(task_file, title, description, status='todo'):
     '''
     Add a new task to the json file
     
-    :param task: task
+    :param task: title
+    :param task: description
     :param task: status
-
-    add_parser.add_argument('title', type=str, help='Task title')
-    add_parser.add_argument('--description', type=str, help='Task description')
-    add_parser.add_argument('--status', type=str, default='todo', choices=status_choices, help='status help')
-    '''
+   '''
 
     task_json = open_json_file(task_file, op='r')
 
     task_id = len(task_json['tasks']) + 1
     ## ensure add a new task with new id, description, status - default "todo"
-    new_task = {'id': task_id,
-                'title': title,
-                'description': description,
-                'status': status
-               }
+
+    created_time = datetime.now()
+    time_format = '%Y-%m-%d %H:%M:%S'
+    new_task = {
+        'id': task_id,
+        'title': title,
+        'description': description,
+        'status': status,
+        'created_at': created_time.strftime(time_format)
+    }
     task_json['tasks'].append(new_task)
 
     open_json_file(task_file, op='w', data=task_json)
@@ -114,17 +135,20 @@ def list_tasks(task_file, list_option="all"):
         title = tasks['title']
         status = tasks['status']
         description = tasks['description']
+        output = f'| {id} |\t| {title} |\t| {status} |'
 
+        print('----------------------------------------')
         if list_option == 'all':
-            print(f'{id}\t{title}\t{status}')
+            print(output)
         elif status == list_option:
-            print(f'{id}\t{title}\t{status}')
+            print(output)
         else: 
             continue
+    print('----------------------------------------')
     return True
 
 
-def update_task(task_file, task_id, status):
+def mark_task(task_file, task_id, status):
 
     task_json = open_json_file(task_file, op='r')
 
@@ -137,14 +161,26 @@ def update_task(task_file, task_id, status):
     return task_json
 
 
-def open_json_file(path, op='r', data=None):
-    if op == 'r':
-        with open(path, op) as f:
-            json_file = json.load(f)
-        return json_file
-    if op == 'w' and data:
-        with open(path, op) as f:
-            json.dump(data, f, indent=4)
+def update_task(task_file, task_id, title, description):
+    updated_time = datetime.now()
+    time_format = '%Y-%m-%d %H:%M:%S'
+    task_json = open_json_file(task_file, op='r')
+
+    for tasks in task_json['tasks']:
+        if task_id == tasks['id']:
+            pre_update = tasks
+            print(f'Pre Update task: {pre_update}')
+            if title:
+                tasks['title'] = title
+            if description:
+                tasks['description'] = description
+            tasks['updated_at'] = updated_time.strftime(time_format)
+            print(f'Updated task #{tasks["id"]}: {tasks}')
+    
+
+def delete_task(task_file, task_id):
+    print('Not Implemented')
+
 
 
 def main():
@@ -162,15 +198,18 @@ def main():
 
     if args.command == 'add':
         add_task(task_file, args.title, args.description, args.status)
+    
+    if args.command == 'update':
+        update_task(task_file, args.id, args.title, args.description)
 
     if args.command == 'list':
         list_tasks(task_file, args.status)
 
-    if args.command == 'update':
-        if not args.status:
-            print('Provide a new status with `--status`')
-        else:
-            update_task(task_file, task_id=args.update, status=args.status)
+    if args.command == 'mark':
+        mark_task(task_file, task_id=args.id, status=args.status)
+    
+    if args.command == 'delete':
+        delete_task(task_file, task_id=args.id)
 
 
 
